4bee23a78523ee9333efdba6fa6d08ad2c5237a1
LuzianeFreitas <freitas.lu@outlook.com>
2019-11-18 10:37:05 -0300

    Ford Fulkerson funcionando
    Ajustar a leitura de arquivos

--- ProblemaFluxoMaximo/src/problemafluxomaximo/Grafo.java
+++ ProblemaFluxoMaximo/src/problemafluxomaximo/Grafo.java
@@ -1,8 +1,84 @@
 package problemafluxomaximo;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+
 
 public class Grafo {
        
+    static final int V = 8;   
+    boolean bfs(int[][] matrizAdj, int s, int t, int[] vizinho) 
+    { 
+        
+        boolean[] visitados = new boolean[V]; 
+        for(int i=0; i<V; ++i) 
+            visitados[i]=false; 
+  
+        
+        LinkedList<Integer> filaPrioridade = new LinkedList<Integer>(); 
+        filaPrioridade.add(s); 
+        visitados[s] = true; 
+        vizinho[s]=-1; 
+  
+       
+        while (filaPrioridade.size()!=0) 
+        { 
+            int u = filaPrioridade.poll(); 
+  
+            for (int v=0; v<V; v++) 
+            { 
+                if (visitados[v]==false && matrizAdj[u][v] > 0) 
+                { 
+                    filaPrioridade.add(v); 
+                    vizinho[v] = u; 
+                    visitados[v] = true; 
+                } 
+            } 
+        } 
+  
+       
+        return (visitados[t] == true);
+    }
     
+    
+    int fordFulkerson(int[][] grafo, int s, int t) 
+    { 
+        int u, v; 
+  
+        
+        int[][] mGrafo = new int[V][V]; 
+  
+        for (u = 0; u < V; u++) 
+            for (v = 0; v < V; v++) 
+                mGrafo[u][v] = grafo[u][v]; 
+  
+        
+        int[] pai = new int[V]; 
+  
+        int fluxoMaximo = 0;  
+        
+        while (bfs(mGrafo, s, t, pai)) 
+        { 
+             
+            int caminhoMaximo = Integer.MAX_VALUE; 
+            for (v=t; v!=s; v=pai[v]) 
+            { 
+                u = pai[v]; 
+                caminhoMaximo = Math.min(caminhoMaximo, mGrafo[u][v]); 
+            } 
+  
+            
+            for (v=t; v != s; v=pai[v]) 
+            { 
+                u = pai[v]; 
+                mGrafo[u][v] -= caminhoMaximo; 
+                mGrafo[v][u] += caminhoMaximo; 
+            } 
+  
+            
+            fluxoMaximo += caminhoMaximo; 
+        } 
+  
+        
+        return fluxoMaximo; 
+    } 
 }
--- ProblemaFluxoMaximo/src/problemafluxomaximo/ProblemaFluxoMaximo.java
+++ ProblemaFluxoMaximo/src/problemafluxomaximo/ProblemaFluxoMaximo.java
@@ -11,37 +11,38 @@ import java.util.Scanner;
 public class ProblemaFluxoMaximo {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+//        File arquivos[];
+//        File diretorio = new File("C:\\Users\\Luziane Freitas\\ProjetosGit\\InterdisciplinarPAA\\max_flow");
+//        arquivos = diretorio.listFiles();
+//        for (int i = 0; i < arquivos.length; i++) {
+//            BufferedReader br = new BufferedReader(new FileReader(arquivos[i]));
+//            String vetor[] = null;
+//            String linha;
+//            int vetorInt[] = null;
+//            ArrayList<Integer> vetorInteiro = new ArrayList(); 
+//            
+//            while (br.ready()) {
+//                linha = br.readLine();
+//                vetor = linha.split(" ");
+//            }
+//            br.close();
+//        }
+           // Let us create a graph shown in the above example 
+        int graph[][] =new int[][] { {0,0,0,100,0,0,0,0},
+            {26,0,0,0,0,0,0,80},
+            {50,40,0,15,0,0,39,0},
+            {80,0,85,0,0,0,0,0},
+            {24,0,0,0,0,0,100,18},
+            {0,0,0,56,99,0,0,90},
+            {0,0,0,0,0,26,0,0},
+            {0,0,0,0,0,0,22,0} 
+                                   }; 
+        Grafo m = new Grafo(); 
+  
+        System.out.println("The maximum possible flow is " +m.fordFulkerson(graph, 0, 7)); 
+  
 
-        File arquivos[];
-        File diretorio = new File("C:\\Users\\Luziane Freitas\\ProjetosGit\\InterdisciplinarPAA\\max_flow");
-        arquivos = diretorio.listFiles();
-        for (int i = 0; i < arquivos.length; i++) {
-            BufferedReader br = new BufferedReader(new FileReader(arquivos[i]));
-            String vetor[] = null;
-            String linha;
-            int vetorInt[] = null;
-            ArrayList<Integer> vetorInteiro = new ArrayList(); 
 
-            while (br.ready()) {
-                linha = br.readLine();
-                vetor = linha.split(" ");
-                for(int j = 0; j < vetor.length; j++){
-                    vetorInteiro.add(Integer.parseInt(vetor[j]));
-                }
-
-            }
-            //Aqui criar o grafo e inicializar a 
-            for(int k = 0; k < vetorInteiro.size(); k++){
-                System.out.printf(vetorInteiro.get(k)+" ");
-            }
-            System.out.println();
-            
-//            g = new Grafo(vetorInteiro.size());
-//            g.inicializaMatriz(vetorInteiro);
-//            g.preencheMatriz(vetorInteiro);
-//            g.exibirmatriz();
-            
-            br.close();
-        }
     }
 }
