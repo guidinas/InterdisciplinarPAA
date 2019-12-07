e0aee403649312f3ac78d91c62beb17b0b9055f3
Waislan Sanches <waislanluis@gmail.com>
2019-12-07 15:02:07 -0300

    Removed: Backtracking

--- Problema da Mochila/src/problema/da/mochila/ProblemaMochila.java
+++ Problema da Mochila/src/problema/da/mochila/ProblemaMochila.java
@@ -8,24 +8,20 @@ import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.List;
-import static problema.da.mochila.Backtracking.start;
-import problema.da.mochila.Item;
 
-public class Knapsack {
+public class ProblemaMochila {
 
-    // items of our problem
-    private Item[] items;
-    // capacity of the bag
-    private int capacity;
+    private final Item[] itens;
+    private final int capacidade;
 
-    public Knapsack(Item[] items, int capacity) {
-        this.items = items;
-        this.capacity = capacity;
+    public ProblemaMochila(Item[] itens, int capacidade) {
+        this.itens = itens;
+        this.capacidade = capacidade;
     }
-
+    /*
     public void display() {
         if (items != null && items.length > 0) {
-            System.out.println("Knapsack problem");
+            System.out.println("ProblemaMochila problem");
             System.out.println("Capacity : " + capacity);
             System.out.println("Items :");
 
@@ -34,46 +30,45 @@ public class Knapsack {
             }
         }
     }
-
-    // we write the solve algorithm
-    public Solution solve() {
-        int NB_ITEMS = items.length;
-        // we use a matrix to store the max value at each n-th item
-        int[][] matrix = new int[NB_ITEMS + 1][capacity + 1];
+    */
+    public Solucao resolve() {
+        int NB_ITEMS = itens.length;
+        // we use a matrix to store the max valor at each n-th item
+        int[][] matrix = new int[NB_ITEMS + 1][capacidade + 1];
 
         // first line is initialized to 0
-        for (int i = 0; i <= capacity; i++) {
+        for (int i = 0; i <= capacidade; i++) {
             matrix[0][i] = 0;
         }
 
         // we iterate on items
         for (int i = 1; i <= NB_ITEMS; i++) {
             // we iterate on each capacity
-            for (int j = 0; j <= capacity; j++) {
-                if (items[i - 1].weight > j) {
+            for (int j = 0; j <= capacidade; j++) {
+                if (itens[i - 1].peso > j) {
                     matrix[i][j] = matrix[i - 1][j];
-                } else // we maximize value at this rank in the matrix
+                } else // we maximize valor at this rank in the matrix
                 {
-                    matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i - 1][j - items[i - 1].weight]
-                            + items[i - 1].value);
+                    matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i - 1][j - itens[i - 1].peso]
+                            + itens[i - 1].valor);
                 }
             }
         }
 
-        int res = matrix[NB_ITEMS][capacity];
-        int w = capacity;
+        int res = matrix[NB_ITEMS][capacidade];
+        int w = capacidade;
         List<Item> itemsSolution = new ArrayList<>();
 
         for (int i = NB_ITEMS; i > 0 && res > 0; i--) {
             if (res != matrix[i - 1][w]) {
-                itemsSolution.add(items[i - 1]);
-                // we remove items value and weight
-                res -= items[i - 1].value;
-                w -= items[i - 1].weight;
+                itemsSolution.add(itens[i - 1]);
+                // we remove items valor and peso
+                res -= itens[i - 1].valor;
+                w -= itens[i - 1].peso;
             }
         }
 
-        return new Solution(itemsSolution, matrix[NB_ITEMS][capacity]);
+        return new Solucao(itemsSolution, matrix[NB_ITEMS][capacidade]);
     }
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
@@ -109,12 +104,12 @@ public class Knapsack {
             for (int j = 0; j < pesos.length; j++){
                 items[j] = new Item(valores[j], pesos[j]);
             }
-            Knapsack knapsack = new Knapsack(items, capacidade);
-            knapsack.display();
+            ProblemaMochila knapsack = new ProblemaMochila(items, capacidade);
+            //knapsack.display();
             System.out.println("---- Backtracking ----");
             //System.out.print("Itens na mochila: ");
-            Solution solution = knapsack.solve();
-            solution.display();
+            Solucao solucao = knapsack.resolve();
+            solucao.printa();
         }
     }
 }
